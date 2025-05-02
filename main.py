6af559ed899be5352d6264e6980a1d33d52646b4
pydevdse <pydev.dse@gmail.com>
2025-05-02 12:58:05 +0300

    tests

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import requests
 from get_headers import get_headers
 
 
+proxy = {"http":"http://brd-customer-hl_2e9cdd80-zone-isp_proxy1:doiu6m99dezi@brd.superproxy.io:33335",
+        "https":"http://brd-customer-hl_2e9cdd80-zone-isp_proxy1:doiu6m99dezi@brd.superproxy.io:33335"}
+
 def main(token):
     # url = "https://api-v2.solscan.io/v2/token/transfer?address=4GvsP1a1jknAjZVwdoCq5ShLGxrVyTgwHneSKYPigUgX&page=2&page_size=10&remove_spam=false&exclude_amount_zero=true"
     url = f"https://api-v2.solscan.io/v2/account?address={token}"
+    url = "https://api.myip.com/"
     headers = get_headers("headers.txt")
     
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, proxies=proxy, headers=headers)
     print(response)
-    # print(response.json())
+    print(response.json())
+    return
     with open(f"{token}.json", "w") as f:
         json.dump(response.json(), f, indent=4, ensure_ascii=False)
     
